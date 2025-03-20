ad9039452ab9fb4fee97c08952daa8685e64691a
Octopodo <orson_wells40@hotmail.com>
2025-03-20 14:44:11 +0100

    Removeed old kt testing suite

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,2 @@
-// import { KT } from 'kt-core';
-// import { KtTestingSuiteTs } from './KtTestingSuiteTs';
-// KT.Module('Test', KtTestingSuiteTs);
-// export { KtTestingSuiteTs };
-
 export { describe, it, expect, getSuites, AE } from './expect';
 export { runTests } from './runner';
