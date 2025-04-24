bc568b23bd4b84c7e7ba63739e40d4d9c2135db9
Octopodo <orson_wells40@hotmail.com>
2025-04-24 13:25:09 +0200

    Include IO matchers in base Matchers

--- src/expect/index.ts
+++ src/expect/index.ts
@@ -1,9 +1,10 @@
 import { createExpect, extendMatchers, type Matcher, Expect } from './core';
 import { jsMatchers } from './matchers/js';
+import { IOMatchers } from './matchers/io';
 import { describe, it, beforeEach, afterEach, getSuites } from '../describe';
 
 export function expect<T>(actual: T): Expect<T> & Matcher<T> {
-    return createExpect(actual, [jsMatchers]);
+    return createExpect(actual, [jsMatchers, IOMatchers]);
 }
 
 export {
