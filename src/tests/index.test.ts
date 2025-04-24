33b685c64bd301b9c12d57a423a75f51f7da2708
Octopodo <orson_wells40@hotmail.com>
2025-04-24 13:26:00 +0200

    Temoved build dependencies and KT-core dependency

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,8 +1,5 @@
 // src/tests/index.test.ts
 import { describe, runTests } from '../index';
-import { KT } from 'kt-core';
-
-KT.Module('Test', describe);
 
 import './baseMatchers.test';
 import './not.test';
