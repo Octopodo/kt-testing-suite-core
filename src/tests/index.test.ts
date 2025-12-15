e9d6444672c4a083fedb45d6f3e115b38577c389
octopodo <orson_wells40@hotmail.com>
2025-12-15 12:42:28 +0100

    feat: Add main test runner entry point to consolidate all tests.

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,13 +1,13 @@
 // src/tests/index.test.ts
 import { describe, runTests } from '../index';
 
-// import './baseMatchers.test';
-// import './not.test';
-// import './toPassAny.test';
-// import './extend.test';
-// import './beforeAfterEach.test';
-// import './beforeAfterAll.test';
-// import './throwError.test';
+import './baseMatchers.test';
+import './not.test';
+import './toPassAny.test';
+import './extend.test';
+import './beforeAfterEach.test';
+import './beforeAfterAll.test';
+import './throwError.test';
 import './jsonReporter.test';
 import './filtering.test';
 runTests();
